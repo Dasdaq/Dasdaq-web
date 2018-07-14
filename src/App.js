7bc106c499fc28e77e0f4ba30a6ba4e0264ea4ac
Frank Wei <frank@frankwei.xyz>
2018-07-14 10:41:05 +0800

    feat: very ugly basic home

--- src/App.js
+++ src/App.js
@@ -1,18 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Layout, Menu, Breadcrumb } from 'antd';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { About } from "./components/about";
+import Home from "./pages/Home";
+import Construction from "./pages/Construction";
 import './App.css';
+const { Header, Content, Footer } = Layout;
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Router basename="/">
+            <div className="container">
+              <Header>
+                <div className="logo" />
+                <Menu
+                  theme="dark"
+                  mode="horizontal"
+                  defaultSelectedKeys={['2']}
+                  style={{ lineHeight: '64px' }}
+                >
+                  <Menu.Item key="1">nav 1</Menu.Item>
+                  <Menu.Item key="2">nav 2</Menu.Item>
+                  <Menu.Item key="3">nav 3</Menu.Item>
+                </Menu>
+              </Header>
+
+              <div className="router-view" 
+              // style={{ margin: "2rem 1rem" }}
+              >
+                <Route exact path="/" component={Home} />
+                <Route path="/about" component={About} />
+                <Route path="/my"
+                  component={() => Construction({ pageName: '我的爱心' })} />
+              </div>
+
+
+            </div>
+        </Router>
+        <Footer style={{ textAlign: 'center' }}>
+          Market ©2018
+        </Footer>
       </div>
     );
   }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,35 @@
+import React from "react"
+
+const logo = 'https://i.loli.net/2018/07/14/5b495a8628987.jpg'
+
+const bannerStyle = {
+    padding: `6rem`,
+    color: `#fafafa`,
+    width: "100%", minHeight: "48rem",
+    background: `url(${logo})`, backgroundPosition: '50% 50%'
+}
+
+const titleStyle = {
+    opacity: 1, transform: `translate(0px, 0px)`,
+    fontSize: `68px`,
+    color: `#fafafa`,
+    fontWeight: 600,
+    lineHeight: `76px`,
+    margin: `8px 0 28px`,
+    letterSpacing: 0
+}
+
+export default function Home() {
+    return (<div className="index-page">
+        <div className="banner" style={bannerStyle}>
+            <h1 style={ titleStyle }>
+                MarketData</h1>
+            <p style={{
+                opacity: 1, transform: `translate(0px, 0px)`, fontSize: `20px`,
+                lineHeight: `40px`,
+            }}>
+                币本位查看法币涨跌</p>
+        </div>
+        {/* <img src={logo} alt="给你比心" style={{ maxWidth: "100%" }} /> */}
+    </div>)
+}
