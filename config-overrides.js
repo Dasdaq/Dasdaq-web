b664d741391be82d2f464256620f798d94d4c886
Frank Wei <frank@frankwei.xyz>
2018-07-16 15:16:13 +0800

    style: update primary-color setting

--- config-overrides.js
+++ config-overrides.js
@@ -2,11 +2,10 @@ const { injectBabelPlugin } = require('react-app-rewired');
 const rewireLess = require('react-app-rewire-less');
 
 module.exports = function override(config, env) {
-    // config = injectBabelPlugin(['import', { libraryName: 'antd', libraryDirectory: 'es', style: 'css' }], config);
     config = injectBabelPlugin(['import', { libraryName: 'antd', style: true }], config);
     config = rewireLess.withLoaderOptions({
         javascriptEnabled: true,
-        modifyVars: { "@primary-color": "#52c41a" },
+        modifyVars: { "@primary-color": "#FFA940" },
     })(config, env);
     return config;
 };
