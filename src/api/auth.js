c09ce8ee31b455c9f34b1cd0fb3300620940300b
Frank Wei <frank@frankwei.xyz>
2018-07-17 17:03:41 +0800

    fix: just export them before declartion

--- src/api/auth.js
+++ src/api/auth.js
@@ -2,17 +2,19 @@ import axios from "axios";
 
 axios.defaults.withCredentials = true;
 
-async function register({ username, password, inviter = '' }) {
+export async function register({ username, password, inviter = '' }) {
     const result = await axios.post('http://api.dasdaq.io/register/', {
         username, password, inviter
     })
     if (result.data.err_code !== 0) {
         const { err_msg } = result.data;
         return new Error(err_msg)
+    } else {
+        return result
     }
 }
 
-async function login({ username, password }) {
+export async function login({ username, password }) {
     const result = await axios.post('http://api.dasdaq.io/login/', {
         username, password
     })
@@ -20,14 +22,9 @@ async function login({ username, password }) {
         const { err_msg } = result.data;
         return new Error(err_msg)
     }
+    return result
 }
 
-async function logout() {
-    axios.get('http://api.dasdaq.io/logout/')
+export function logout() {
+    return axios.get('http://api.dasdaq.io/logout/')
 }
-
-export default {
-    register,
-    login,
-    logout
-} 
