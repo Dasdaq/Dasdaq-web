007ab80114be9fe0dada2c2154623699fda0af43
Frank Wei <frank@frankwei.xyz>
2018-07-19 17:00:09 +0800

    feat: set default crypto in nav

--- src/containers/VisiableHeader.jsx
+++ src/containers/VisiableHeader.jsx
@@ -1,18 +1,19 @@
 import { connect } from 'react-redux'
 import { withRouter } from "react-router-dom";
-import { setLanguage, setTheme } from '../actions'
+import { setLanguage, setTheme, setCrypto } from '../actions'
 import Header from '../components/Header'
 
 
 
 const mapStateToProps = state => {
-  const {lang, theme} = state
-  return {lang, theme}
+  const {lang, theme, crypto} = state
+  return {lang, theme, crypto}
 }
 
 const mapDispatchToProps = dispatch => ({
     setLanguage: code => dispatch(setLanguage(code)),
-    setTheme: theme => dispatch(setTheme(theme))
+    setTheme: theme => dispatch(setTheme(theme)),
+    setCrypto: code => dispatch(setCrypto(code))
 })
 
 export default withRouter(
