d9bfecff7db975b02cd9e0edfdc6a5bde124e06a
Frank Wei <frank@frankwei.xyz>
2018-07-23 09:18:19 +0800

    feat: redux action for login and logout

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux'
 import lang from './lang'
 import theme from './theme'
 import crypto from './userCrypto'
+import user from './user'
 
 export default combineReducers({
     lang,
     theme,
-    crypto
+    crypto,
+    user
 })
