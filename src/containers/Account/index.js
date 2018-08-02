42deacd768494f6d3de0be318d2f3d7a2f90ea5b
Frank Wei <frank@frankwei.xyz>
2018-08-03 00:07:12 +0800

    fix: disable dynamic import for Login now

--- src/containers/Account/index.js
+++ src/containers/Account/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Loadable from 'react-loadable';
+import Login from './Login'
 
 const LoadingMessage = (name) => (<div> Loading {name} modules, Please wait</div>)
 
 // 😄 Magic Happened! Dynamic import and Async Load Components
-export const Login = Loadable({
-  loader: () => import('./Login'),
-  loading: () => LoadingMessage('Login')
-});
+// export const Login = Loadable({
+//   loader: () => import('./Login'),
+//   loading: () => LoadingMessage('Login')
+// });
 
 export const User = Loadable({
   loader: () => import('./User'),
