4a21f4924b845ddbaf0ec1ca51bad99e9d0404db
Frank Wei <frank@frankwei.xyz>
2018-08-05 01:20:14 +0800

    fix: setting default crypto and lang not the theme

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -63,7 +63,7 @@ export default function Navbar(props) {
               onCancel={() => setSettingVisible(false)}
             >
                 选择单位：
-                <Select defaultValue={crypto} style={{ width: 120 }} onChange={(value) => setTheme(value)}>
+                <Select defaultValue={crypto} style={{ width: 120 }} onChange={(value) => setCrypto(value)}>
                   <Option value="BTC">Bitcoin</Option>
                   <Option value="ETH">Ethereum</Option>
                   <Option value="EOS">EOS</Option>
@@ -71,7 +71,7 @@ export default function Navbar(props) {
                 <br />
                 <br />
                 选择语言：
-                <Select defaultValue={langList[lang]} style={{ width: 120 }} onChange={(value) => setTheme(value)}>
+                <Select defaultValue={langList[lang]} style={{ width: 120 }} onChange={(value) => setLanguage(value)}>
                   <Option value="SWITCH_TO_CHINESE">中文</Option>
                   <Option value="SWITCH_TO_ENGLISH">English</Option>
                   <Option value="SWITCH_TO_JAPANESE">日本語</Option>
@@ -88,27 +88,3 @@ export default function Navbar(props) {
         </Menu>
     )
 }
-
-
-
-            // <SubMenu
-            //     style={{ float: 'right' }}
-            //     title={<span> {theme} </span>}>
-            //     <Menu.Item onClick={() => setTheme('SWITCH_TO_DARK')}> DARK </Menu.Item>
-            //     <Menu.Item onClick={() => setTheme('SWITCH_TO_LIGHT')}> LIGHT </Menu.Item>
-            // </SubMenu>
-            // <SubMenu
-            //     style={{ float: 'right' }}
-            //     title={<span><Icon type="global" /><span> {langList[lang]} </span></span>}>
-            //     <Menu.Item onClick={() => setLanguage('SWITCH_TO_CHINESE')}>中文</Menu.Item>
-            //     <Menu.Item onClick={() => setLanguage('SWITCH_TO_ENGLISH')}>English</Menu.Item>
-            //     <Menu.Item onClick={() => setLanguage('SWITCH_TO_JAPANESE')}>日本語</Menu.Item>
-            //     <Menu.Item onClick={() => setLanguage('SWITCH_TO_KOREAN')}>한국말</Menu.Item>
-            // </SubMenu>
-            // <SubMenu
-            //     style={{ float: 'right' }}
-            //     title={<span><Icon type="bank" /><span> {navbarI18n('unit')}: {crypto}</span></span>}>
-            //     <Menu.Item onClick={() => setCrypto('BTC')}>Bitcoin</Menu.Item>
-            //     <Menu.Item onClick={() => setCrypto('ETH')}>Ethereum</Menu.Item>
-            //     <Menu.Item onClick={() => setCrypto('EOS')}>EOS</Menu.Item>
-            // </SubMenu>
